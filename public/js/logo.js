4f23150fc3b903a5c4bd56ed4587d37316938a20
Jan Christian Bernabe <janchristianbernabe@gmail.com>
2015-12-08 11:37:33 -0600

    Rotating updated backgrounds + new logo

--- public/js/logo.js
+++ public/js/logo.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var backgroundImages = ["ireland-1.jpg", "ireland-2.jpg", "madison-1.jpg", "madison-2.jpg", "madison-3.jpg", "nature-1.jpg", "nature-2.jpg", "nature-3.jpg", "nature-4.jpg", "seattle-1.jpg", "seattle-2.jpg"];
   $("body").css("background-image", "url(/img/background/"+pickElement(backgroundImages)+")");
-  $('<img src="/img/logo/thoughtful-logo-3.png">').load(function() {
+  $('<img src="/img/logo/thoughtful-logo-4.png">').load(function() {
   $(this).appendTo('.main-logo');
 });
 });
